2f7e15619f5a604f8bbe8963d796fc32bed46325
YanzheL <lee.yanzhe@yanzhe.org>
2019-11-09 00:47:43 +0800

    add @Component to enable auto-scan

--- src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/parsing/PDNSLogFieldSetMapper.kt
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/parsing/PDNSLogFieldSetMapper.kt
@@ -2,6 +2,8 @@ package com.hitnslab.dnssecurity.pdnsdataloader.parsing
 
 import com.hitnslab.dnssecurity.pdnsdataloader.model.PDnsData
 import org.springframework.batch.item.file.mapping.FieldSetMapper
+import org.springframework.stereotype.Component
 
+@Component
 interface PDNSLogFieldSetMapper : FieldSetMapper<PDnsData> {
 }
