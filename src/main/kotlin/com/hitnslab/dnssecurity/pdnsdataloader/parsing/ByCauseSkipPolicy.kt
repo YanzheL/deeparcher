e648e2f98643c394631443ea10ee5d287df189f1
YanzheL <lee.yanzhe@yanzhe.org>
2019-11-09 17:20:31 +0800

    log for debug

--- src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/parsing/ByCauseSkipPolicy.kt
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/parsing/ByCauseSkipPolicy.kt
@@ -1,9 +1,18 @@
 package com.hitnslab.dnssecurity.pdnsdataloader.parsing
 
+import mu.KotlinLogging
 import org.springframework.batch.core.step.skip.SkipPolicy
 import kotlin.reflect.KClass
 
 class ByCauseSkipPolicy<T : Throwable>(private val clazz: KClass<T>) : SkipPolicy {
 
-    override fun shouldSkip(t: Throwable, skipCount: Int) = clazz.isInstance(t.cause)
+    private val logger = KotlinLogging.logger {}
+
+    override fun shouldSkip(t: Throwable, skipCount: Int): Boolean {
+        val ret = clazz.isInstance(t.cause)
+        if (ret) {
+            logger.debug { "Skipped exception <${t.cause}>" }
+        }
+        return ret
+    }
 }
