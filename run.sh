1b2d00b7549ad08eb6f0a0a74e3e993de695b472
YanzheL <lee.yanzhe@yanzhe.org>
2019-11-10 23:26:02 +0800

    refine

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 JAR=$1
-PATTERN=$2
-JVM_OPTS="-server -XX:+HeapDumpOnOutOfMemoryError -Xmx12g -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:gc*:gc.log"
-java $JVM_OPTS $EXTRA_OPTS -jar "$JAR" LoadLogFromFileToDBJobConfig loadLogFromFileToDB pattern="$PATTERN" table=raw_pdns_log min-pool-size=24 max-pool-size=64
+TABLE=$2
+PATTERN=$3
+JVM_OPTS="-server -XX:+HeapDumpOnOutOfMemoryError -Xss256k -Xmx10g -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:gc*:gc.log"
+java \
+	$JVM_OPTS \
+	$EXTRA_OPTS \
+	-jar "$JAR" \
+	LoadLogFromFileToDBJobConfig loadLogFromFileToDB pattern="$PATTERN" table="$TABLE"
