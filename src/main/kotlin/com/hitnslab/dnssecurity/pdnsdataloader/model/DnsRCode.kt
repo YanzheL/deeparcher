1c0840b18fb239158dde6930c82f9908a5dfac32
YanzheL <lee.yanzhe@yanzhe.org>
2019-11-12 19:08:35 +0800

    dns protocol codes

---
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/model/DnsRCode.kt
@@ -0,0 +1,19 @@
+package com.hitnslab.dnssecurity.pdnsdataloader.model
+
+/**
+ * See [DNS Parameters](https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml)
+ */
+enum class DnsRCode(val value: Int) {
+    NOERROR(0),
+    FORMERR(1),
+    SERVFAIL(2),
+    NXDOMAIN(3),
+    NOTIMP(4),
+    REFUSED(5),
+    YXDOMAIN(6),
+    YXRRSET(7),
+    NXRRSET(8),
+    NOTAUTH(9),
+    NOTZONE(10),
+    DSOTYPENI(11)
+}
