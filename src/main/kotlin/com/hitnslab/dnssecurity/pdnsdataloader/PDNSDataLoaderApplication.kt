d56f391e733dc04704cabfebd751da7a935b8855
YanzheL <lee.yanzhe@yanzhe.org>
2020-01-28 09:10:39 +0800

    Set proxyBeanMethods = false
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/PDNSDataLoaderApplication.kt
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/PDNSDataLoaderApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 @EnableConfigurationProperties
 class PDNSDataLoaderApplication
 
