229726487a90efac369282ae122b42aeefe568cc
YanzheL <lee.yanzhe@yanzhe.org>
2020-02-08 22:24:54 +0800

    Use kotlin `isSuperclassOf` instead of `isInstance`
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/processing/ByCauseSkipPolicy.kt
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/processing/ByCauseSkipPolicy.kt
@@ -3,15 +3,17 @@ package com.hitnslab.dnssecurity.pdnsdataloader.processing
 import mu.KotlinLogging
 import org.springframework.batch.core.step.skip.SkipPolicy
 import kotlin.reflect.KClass
+import kotlin.reflect.full.isSuperclassOf
 
 class ByCauseSkipPolicy<T : Throwable>(private val clazz: KClass<T>) : SkipPolicy {
 
     private val logger = KotlinLogging.logger {}
 
     override fun shouldSkip(t: Throwable, skipCount: Int): Boolean {
-        val ret = clazz.isInstance(t.cause)
+        val target = if (t.cause == null) t else t.cause
+        val ret = clazz.isSuperclassOf(target!!::class)
         if (ret) {
-            logger.debug { "Skipped exception <${t.cause}>" }
+            logger.debug { "Skipped exception <${target}>" }
         }
         return ret
     }
