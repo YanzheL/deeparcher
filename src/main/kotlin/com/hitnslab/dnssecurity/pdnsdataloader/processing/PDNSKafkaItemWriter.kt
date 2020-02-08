7ae71e30e9a1a8aeaa145a1789cc1667c263859c
YanzheL <lee.yanzhe@yanzhe.org>
2020-02-09 04:05:03 +0800

    Use async callback in KafkaTemplate send result
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/processing/PDNSKafkaItemWriter.kt
+++ src/main/kotlin/com/hitnslab/dnssecurity/pdnsdataloader/processing/PDNSKafkaItemWriter.kt
@@ -5,32 +5,47 @@ import mu.KotlinLogging
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.batch.item.ItemWriter
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.util.concurrent.ListenableFutureCallback
+import java.util.concurrent.atomic.AtomicInteger
 
 class PDNSKafkaItemWriter(val kafkaTemplate: KafkaTemplate<String, PDnsData>) : ItemWriter<PDnsData> {
 
     private val logger = KotlinLogging.logger {}
 
+    var sentCount = AtomicInteger(0)
+
+    var failedCount = AtomicInteger(0)
+
+    var successInterval = 100000
+
+    var failureInterval = 100000
+
+    private val itemCallback = object : ListenableFutureCallback<SendResult<*, *>> {
+        override fun onSuccess(result: SendResult<*, *>?) {
+            val current = sentCount.incrementAndGet()
+            if (current >= successInterval) {
+                logger.info { "Successfully wrote $current items to kafka" }
+                sentCount.addAndGet(-current)
+            }
+        }
+
+        override fun onFailure(ex: Throwable) {
+            val current = failedCount.incrementAndGet()
+            if (current % failureInterval == 0) {
+                logger.error { "Failed to send $current items with exception <$ex>" }
+            }
+        }
+    }
+
     override fun write(items: MutableList<out PDnsData>) {
-        val results = items
-                .map {
+        val futures = items
+                .forEach {
                     //            val headers = mutableListOf<Header>()
                     val record = ProducerRecord<String, PDnsData>(kafkaTemplate.defaultTopic, null, it.queryTime.epochSecond, it.domain, it)
-                    kafkaTemplate.send(record)
+                    val future = kafkaTemplate.send(record)
+                    future.addCallback(itemCallback)
                 }
-                .asSequence()
-                .map {
-                    val sendResult = it.get()
-                    if (!it.isDone) {
-                        logger.error { "Failed to send <${sendResult.producerRecord.value()}> to Kafka" }
-                    }
-                    it
-                }
-                .filterNot { it.isDone }
-                .toList()
-        if (results.isEmpty()) {
-            logger.info { "Successfully wrote ${items.size} items to kafka" }
-        } else {
-            logger.warn { "Fail to wrote ${results.size} items to kafka" }
-        }
+
     }
 }
