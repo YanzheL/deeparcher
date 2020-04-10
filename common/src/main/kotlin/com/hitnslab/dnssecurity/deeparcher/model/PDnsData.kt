52b4df22070f961c303788e7e47f5be39ecea294
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-10 23:02:41 +0800

    Implement Builder pattern for PDnsData Type
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/model/PDnsData.kt
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/model/PDnsData.kt
@@ -1,30 +1,161 @@
 package com.hitnslab.dnssecurity.deeparcher.model
 
+import com.google.common.net.InetAddresses
 import com.google.common.net.InternetDomainName
+import com.hitnslab.dnssecurity.deeparcher.error.PDNSInvalidFieldException
+import mu.KotlinLogging
+import java.net.InetAddress
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 
 data class PDnsData(
         val queryTime: Instant,
         val domain: String,
-        val queryType: String,
-        val replyCode: String
+        val queryType: DnsQueryType,
+        val replyCode: DnsRCode,
+        val topPrivateDomain: String,
+        val clientIp: InetAddress?,
+        val ips: Set<InetAddress>?,
+        val cnames: Set<String>?
 ) {
 
-    var clientIp: String? = null
+    class Builder {
+        private var queryTime: Instant? = null
+        private var domain: String? = null
+        private var queryType: DnsQueryType? = null
+        private var replyCode: DnsRCode? = null
+        private var topPrivateDomain: String? = null
+        private var clientIp: InetAddress? = null
+        private var ips: MutableSet<InetAddress>? = null
+        private var cnames: MutableSet<String>? = null
+        private val logger = KotlinLogging.logger {}
+        var error: Throwable? = null
+            private set
 
-    var topPrivateDomain: String? = null
-        private set
-
-    val ips = mutableSetOf<String>()
-
-    val cnames = mutableSetOf<String>()
-
-    init {
-        topPrivateDomain = try {
-            InternetDomainName.from(domain).topPrivateDomain().toString()
-        } catch (e: Exception) {
-            null
+        fun queryTime(value: String, fmt: DateTimeFormatter) = apply {
+            queryTime = try {
+                ZonedDateTime.of(
+                        LocalDateTime.parse(value, fmt),
+                        ZoneId.systemDefault()
+                ).toInstant()
+            } catch (e: Exception) {
+                logger.warn { "Invalid queryTime<$value>" }
+                null
+            }
         }
+
+        fun queryTime(value: Long) = apply {
+            queryTime = try {
+                Instant.ofEpochMilli(value)
+            } catch (e: Exception) {
+                logger.warn { "Invalid queryTime<$value>" }
+                null
+            }
+        }
+
+        fun queryTime(value: Instant) = apply { queryTime = value }
+        fun domain(value: String) = apply { domain = value.toLowerCase() }
+        fun queryType(value: String) = apply {
+            queryType = try {
+                DnsQueryType.valueOf(value)
+            } catch (e: Exception) {
+                logger.warn { "Invalid queryType<$value>" }
+                null
+            }
+        }
+
+        fun queryType(value: Int) = apply {
+            queryType = try {
+                DnsQueryType.fromInt(value)
+            } catch (e: Exception) {
+                logger.warn { "Invalid queryType<$value>" }
+                null
+            }
+        }
+
+        fun replyCode(value: String) = apply {
+            replyCode = try {
+                DnsRCode.valueOf(value)
+            } catch (e: Exception) {
+                logger.warn { "Invalid replyCode<$value>" }
+                null
+            }
+        }
+
+        fun replyCode(value: Int) = apply {
+            replyCode = try {
+                DnsRCode.fromInt(value)
+            } catch (e: Exception) {
+                logger.warn { "Invalid replyCode<$value>" }
+                null
+            }
+        }
+
+        fun topPrivateDomain(value: String) = apply { topPrivateDomain = value.toLowerCase() }
+        fun clientIp(value: String) = apply { clientIp = InetAddress.getByName(value) }
+        fun clientIp(value: InetAddress) = apply { clientIp = value }
+        fun clientIp(value: ByteArray) = apply { clientIp = InetAddress.getByAddress(value) }
+        fun setIps(value: MutableSet<InetAddress>) = apply { ips = value }
+        fun addIp(value: String) = apply {
+            if (InetAddresses.isInetAddress(value)) {
+                addIp(InetAddress.getByName(value))
+            } else {
+                logger.warn { "Invalid IP<$value>" }
+            }
+        }
+
+        fun addIp(value: InetAddress) = apply {
+            if (ips == null) {
+                ips = mutableSetOf()
+            }
+            ips!!.add(value)
+        }
+
+        fun addIp(value: ByteArray) = addIp(InetAddress.getByAddress(value))
+        fun setCNames(value: MutableSet<String>) = apply { cnames = value }
+        fun addCName(value: String) = apply {
+            if (cnames == null) {
+                cnames = mutableSetOf()
+            }
+            cnames!!.add(value)
+        }
+
+        fun build(): PDnsData? {
+            if (queryTime == null) {
+                error = PDNSInvalidFieldException("queryTime cannot be null")
+                return null
+            }
+            if (domain == null) {
+                error = PDNSInvalidFieldException("domain cannot be null")
+                return null
+            }
+            if (queryType == null) {
+                error = PDNSInvalidFieldException("queryType cannot be null")
+                return null
+            }
+            if (replyCode == null) {
+                error = PDNSInvalidFieldException("replyCode cannot be null")
+                return null
+            }
+            if (topPrivateDomain == null) {
+                topPrivateDomain = InternetDomainName.from(domain).topPrivateDomain().toString()
+            }
+            error = null
+            return PDnsData(
+                    queryTime!!,
+                    domain!!,
+                    queryType!!,
+                    replyCode!!,
+                    topPrivateDomain!!,
+                    clientIp,
+                    ips,
+                    cnames
+            )
+        }
+
     }
 }
--- pdns-data-loader/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/pdnsdataloader/error/PDNSInvalidFieldException.kt
+++
@@ -1,8 +0,0 @@
-package com.hitnslab.dnssecurity.deeparcher.pdnsdataloader.error
-
-
-class PDNSInvalidFieldException(message: String?, cause: Throwable?) : PDNSParseException(message, cause) {
-    constructor () : this(null, null)
-    constructor (s: String) : this(s, null)
-    constructor (cause: Throwable) : this(null, cause)
-}
