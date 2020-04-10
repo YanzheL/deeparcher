dc0b354e7141d510e992f9fe29159a249ee8531c
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-10 23:09:20 +0800

    Adapt to builder pattern in PDnsData Serdes
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/serde/PDnsDeserializer.kt
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/serde/PDnsDeserializer.kt
@@ -1,43 +1,52 @@
 package com.hitnslab.dnssecurity.deeparcher.serde
 
 import com.hitnslab.dnssecurity.deeparcher.api.proto.PDnsDataProto
-import com.hitnslab.dnssecurity.deeparcher.model.DnsQueryType
-import com.hitnslab.dnssecurity.deeparcher.model.DnsRCode
 import com.hitnslab.dnssecurity.deeparcher.model.PDnsData
 import org.apache.kafka.common.serialization.Deserializer
 import java.net.InetAddress
 import java.nio.ByteBuffer
-import java.time.Instant
 
 class PDnsDeserializer : Deserializer<PDnsData> {
     override fun deserialize(topic: String?, data: ByteArray?): PDnsData {
         val parsed = PDnsDataProto.PDnsData.parseFrom(data)
-        val a = parsed.rCnamesList
-        val pdnsData = PDnsData(
-                queryTime = Instant.ofEpochMilli(parsed.qTime),
-                domain = parsed.fqdn,
-                queryType = DnsQueryType.fromInt(parsed.qType).name,
-                replyCode = DnsRCode.fromInt(parsed.rCode).name
-        )
-        pdnsData.clientIp = InetAddress.getByAddress(parsed.clientIp.toByteArray()).hostAddress
-        parseIpFromBytes(parsed.rIpv4Addrs.toByteArray(), 4, pdnsData.ips)
-        parseIpFromBytes(parsed.rIpv6Addrs.toByteArray(), 16, pdnsData.ips)
-        pdnsData.cnames.addAll(parsed.rCnamesList)
-        return pdnsData
+        val builder = PDnsData.Builder()
+                .queryTime(parsed.qTime)
+                .domain(parsed.fqdn)
+                .queryType(parsed.qType)
+                .replyCode(parsed.rCode)
+                .topPrivateDomain(parsed.domain)
+                .clientIp(parsed.clientIp.toByteArray())
+
+        val ips = mutableSetOf<InetAddress>()
+        parseIpFromBytes(parsed.rIpv4Addrs.toByteArray(), 4, ips)
+        parseIpFromBytes(parsed.rIpv6Addrs.toByteArray(), 16, ips)
+        builder.setIps(ips)
+        val cnames = mutableSetOf<String>()
+        cnames.addAll(parsed.rCnamesList)
+        builder.setCNames(cnames)
+        val data = builder.build()
+        val err = builder.error
+        if (data != null) {
+            return data
+        }
+        if (err != null) {
+            throw err
+        }
+        throw Exception("Unknown error")
     }
 
-    private fun parseIpFromBytes(bytesIn: ByteArray, width: Int, ipOut: MutableCollection<String>) {
+    private fun parseIpFromBytes(bytesIn: ByteArray, width: Int, ipOut: MutableCollection<InetAddress>) {
         if (bytesIn.isEmpty()) {
             return
         }
         if (bytesIn.size % width != 0) {
-            throw Exception("IP Address Bytes <$bytesIn> has incorrect length <${bytesIn.size}>.")
+            throw IllegalArgumentException("IP Address Bytes <$bytesIn> has incorrect length <${bytesIn.size}>.")
         }
         val buffer = ByteBuffer.allocate(width)
         for ((i, byte) in bytesIn.withIndex()) {
             buffer.put(byte)
             if (i != 0 && (i + 1) % width == 0) {
-                ipOut.add(InetAddress.getByAddress(buffer.array()).hostAddress)
+                ipOut.add(InetAddress.getByAddress(buffer.array()))
                 buffer.clear()
             }
         }
--- common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/serde/PDnsSerializer.kt
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/serde/PDnsSerializer.kt
@@ -2,13 +2,10 @@ package com.hitnslab.dnssecurity.deeparcher.serde
 
 import com.google.protobuf.ByteString
 import com.hitnslab.dnssecurity.deeparcher.api.proto.PDnsDataProto
-import com.hitnslab.dnssecurity.deeparcher.model.DnsQueryType
-import com.hitnslab.dnssecurity.deeparcher.model.DnsRCode
 import com.hitnslab.dnssecurity.deeparcher.model.PDnsData
 import org.apache.kafka.common.serialization.Serializer
 import java.net.Inet4Address
 import java.net.Inet6Address
-import java.net.InetAddress
 
 class PDnsSerializer : Serializer<PDnsData> {
     override fun serialize(topic: String?, data: PDnsData?): ByteArray {
@@ -16,18 +13,19 @@ class PDnsSerializer : Serializer<PDnsData> {
                 .newBuilder()
                 .setQTime(data!!.queryTime.toEpochMilli())
                 .setDomain(data.topPrivateDomain)
-                .setQType(DnsQueryType.valueOf(data.queryType).value)
-                .setRCode(DnsRCode.valueOf(data.replyCode).value)
+                .setQType(data.queryType.value)
+                .setRCode(data.replyCode.value)
                 .setFqdn(data.domain)
-                .setClientIp(ByteString.copyFrom(InetAddress.getByName(data.clientIp).address))
+                .setClientIp(ByteString.copyFrom(data.clientIp?.address))
         val allIpv4Bytes = mutableListOf<Byte>()
         val allIpv6Bytes = mutableListOf<Byte>()
-        for (ip in data.ips) {
-            val inetAddr = InetAddress.getByName(ip)
-            val bytes = inetAddr.address.toList()
-            when (inetAddr) {
-                is Inet4Address -> allIpv4Bytes.addAll(bytes)
-                is Inet6Address -> allIpv6Bytes.addAll(bytes)
+        if (data.ips != null) {
+            for (ip in data.ips) {
+                val bytes = ip.address.toList()
+                when (ip) {
+                    is Inet4Address -> allIpv4Bytes.addAll(bytes)
+                    is Inet6Address -> allIpv6Bytes.addAll(bytes)
+                }
             }
         }
         builder.rIpv4Addrs = ByteString.copyFrom(allIpv4Bytes.toByteArray())
