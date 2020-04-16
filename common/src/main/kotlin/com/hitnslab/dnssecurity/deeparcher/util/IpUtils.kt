7362ada28c0c7518e2e589083597f3ef75623f67
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-16 22:29:29 +0800

    Packed IP Address bytes to Number type
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/util/IpUtils.kt
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/util/IpUtils.kt
@@ -29,3 +29,29 @@ fun parseIpFromBytes(bytesIn: ByteArray, width: Int, ipOut: MutableCollection<In
 fun parseIpFromBytes(bytesIn: ByteBuffer, width: Int, ipOut: MutableCollection<InetAddress>) {
     return parseIpFromBytes(Unpooled.wrappedBuffer(bytesIn), width, ipOut)
 }
+
+fun <R : Number> packIpFromBytes(bytesIn: ByteArray, width: Int, ipOut: MutableList<R>) {
+    return packIpFromBytes(Unpooled.wrappedBuffer(bytesIn), width, ipOut)
+}
+
+fun <R : Number> packIpFromBytes(bytesIn: ByteBuffer, width: Int, ipOut: MutableList<R>) {
+    return packIpFromBytes(Unpooled.wrappedBuffer(bytesIn), width, ipOut)
+}
+
+fun <R : Number> packIpFromBytes(bytesIn: ByteBuf, width: Int, ipOut: MutableList<R>) {
+    val view = bytesIn.slice()
+    val size = view.readableBytes()
+    if (size == 0) {
+        return
+    }
+    if (size % width != 0) {
+        throw IllegalArgumentException("IP Address Bytes <$bytesIn> has incorrect length <${size}>.")
+    }
+    while (view.readableBytes() != 0) {
+        if (width == 4) {
+            ipOut.add(view.readInt() as R)
+        } else {
+            ipOut.add(view.readLong() as R)
+        }
+    }
+}
