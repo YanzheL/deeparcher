b7cfa7428634a3375485f7299b1f66aa760e74e7
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-30 02:00:13 +0800

    Add generated python and C++ protobuf files
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- common/proto_gen.sh
+++ common/proto_gen.sh
@@ -2,4 +2,11 @@
 SRC_DIR=src/main/kotlin
 DST_DIR=$SRC_DIR
 PROTO_PKG=com/hitnslab/dnssecurity/deeparcher/api/proto
-protoc -I="$SRC_DIR" --java_out="$DST_DIR" $SRC_DIR/$PROTO_PKG/*.proto
+protoc -I="$SRC_DIR" \
+  --java_out="$DST_DIR" \
+  --python_out="$DST_DIR" \
+  --cpp_out="$DST_DIR" \
+  $SRC_DIR/$PROTO_PKG/*.proto && \
+mv $SRC_DIR/$PROTO_PKG/*.py $SRC_DIR/$PROTO_PKG/generated/ && \
+mv $SRC_DIR/$PROTO_PKG/*.cc $SRC_DIR/$PROTO_PKG/generated/ && \
+mv $SRC_DIR/$PROTO_PKG/*.h $SRC_DIR/$PROTO_PKG/generated/
