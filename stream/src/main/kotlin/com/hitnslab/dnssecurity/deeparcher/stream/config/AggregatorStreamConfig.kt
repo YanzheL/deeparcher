09b902487432b6c316d6ab4df9fe9d954a88f418
YanzheL <lee.yanzhe@yanzhe.org>
2020-04-30 00:21:16 +0800

    Aggregate CNAME field
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- stream/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/stream/config/AggregatorStreamConfig.kt
+++ stream/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/stream/config/AggregatorStreamConfig.kt
@@ -33,54 +33,69 @@ class AggregatorStreamConfig : AppStreamConfigurer() {
     @Bean
     fun aggregatorStream(builder: StreamsBuilder): KStream<*, *> {
         val src = builder.stream(
-                properties.input.path,
-                Consumed.with(
-                        Serdes.String(),
-                        GenericSerde(PDnsProtoSerializer::class, PDnsProtoDeserializer::class)
-                )
+            properties.input.path,
+            Consumed.with(
+                Serdes.String(),
+                GenericSerde(PDnsProtoSerializer::class, PDnsProtoDeserializer::class)
+            )
         )
         val updates = src
-                .groupByKey()
-                .aggregate(
-                        { DomainIPAssocDetailProto.DomainIPAssocDetail.getDefaultInstance() },
-                        { k, v, agg ->
-                            val ab = DomainIPAssocDetailProto.DomainIPAssocDetail
-                                    .newBuilder()
-                                    .mergeFrom(agg)
-                            if (ab.fqdn.isEmpty()) {
-                                ab.fqdn = k
+            .groupByKey()
+            .aggregate(
+                { DomainIPAssocDetailProto.DomainIPAssocDetail.getDefaultInstance() },
+                { k, v, agg ->
+                    val aggBuilder = DomainIPAssocDetailProto.DomainIPAssocDetail
+                        .newBuilder()
+                        .mergeFrom(agg)
+                    if (aggBuilder.fqdn.isEmpty()) {
+                        aggBuilder.fqdn = k
+                    }
+                    if (aggBuilder.domain.isEmpty()) {
+                        aggBuilder.domain = v.domain
+                    }
+                    if (!v.rIpv4Addrs.isEmpty) {
+                        if (aggBuilder.ipv4Addrs.isEmpty) {
+                            aggBuilder.ipv4Addrs = v.rIpv4Addrs
+                        } else {
+                            val bufSet = ByteBufSet(aggBuilder.ipv4Addrs.asReadOnlyByteBuffer())
+                            bufSet.addAll(Unpooled.wrappedBuffer(v.rIpv4Addrs.asReadOnlyByteBuffer()), 4)
+                            if (bufSet.dirty) {
+                                aggBuilder.ipv4Addrs = ByteString.copyFrom(bufSet.buffer.nioBuffer())
                             }
-                            if (ab.domain.isEmpty()) {
-                                ab.domain = v.domain
+                            bufSet.release()
+                        }
+                    }
+                    if (!v.rIpv6Addrs.isEmpty) {
+                        if (aggBuilder.ipv6Addrs.isEmpty) {
+                            aggBuilder.ipv6Addrs = v.rIpv6Addrs
+                        } else {
+                            val bufSet = ByteBufSet(aggBuilder.ipv6Addrs.asReadOnlyByteBuffer())
+                            bufSet.addAll(Unpooled.wrappedBuffer(v.rIpv6Addrs.asReadOnlyByteBuffer()), 16)
+                            if (bufSet.dirty) {
+                                aggBuilder.ipv6Addrs = ByteString.copyFrom(bufSet.buffer.nioBuffer())
                             }
-                            if (ab.ipv4Addrs.isEmpty) {
-                                ab.ipv4Addrs = v.rIpv4Addrs
-                            } else {
-                                val bufSet = ByteBufSet(ab.ipv4Addrs.asReadOnlyByteBuffer())
-                                bufSet.addAll(Unpooled.wrappedBuffer(v.rIpv4Addrs.asReadOnlyByteBuffer()), 4)
-                                if (bufSet.dirty) {
-                                    ab.ipv4Addrs = ByteString.copyFrom(bufSet.buffer.nioBuffer())
-                                }
-                                bufSet.release()
+                            bufSet.release()
+                        }
+                    }
+                    if (v.rCnamesCount != 0) {
+                        if (aggBuilder.cnamesCount == 0) {
+                            aggBuilder.addAllCnames(v.rCnamesList)
+                        } else {
+                            val cnameSet = aggBuilder.cnamesList.toMutableSet()
+                            if (cnameSet.addAll(v.rCnamesList)) {
+                                aggBuilder.clearCnames()
+                                aggBuilder.addAllCnames(cnameSet)
                             }
-                            if (ab.ipv6Addrs.isEmpty) {
-                                ab.ipv6Addrs = v.rIpv6Addrs
-                            } else {
-                                val bufSet = ByteBufSet(ab.ipv6Addrs.asReadOnlyByteBuffer())
-                                bufSet.addAll(Unpooled.wrappedBuffer(v.rIpv6Addrs.asReadOnlyByteBuffer()), 16)
-                                if (bufSet.dirty) {
-                                    ab.ipv6Addrs = ByteString.copyFrom(bufSet.buffer.nioBuffer())
-                                }
-                                bufSet.release()
-                            }
-                            ab.build()
-                        },
-                        Materialized.with(
-                                Serdes.String(),
-                                GenericSerde(DomainIPAssocDetailProtoSerializer::class, DomainIPAssocDetailProtoDeserializer::class)
-                        )
+                        }
+                    }
+                    aggBuilder.build()
+                },
+                Materialized.with(
+                    Serdes.String(),
+                    GenericSerde(DomainIPAssocDetailProtoSerializer::class, DomainIPAssocDetailProtoDeserializer::class)
                 )
-                .toStream()
+            )
+            .toStream()
         properties.output.forEach {
             configSinks(updates, it.type, it.path, it.options)
         }
