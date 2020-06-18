25d943cbe3f440dc89a26b2df9ab709c93d2043e
YanzheL <lee.yanzhe@yanzhe.org>
2020-06-18 15:05:25 +0800

    Update mongodb config

--- deploy/kubernetes/infra/db/mongodb/uninstall.sh
+++ deploy/kubernetes/infra/db/mongodb/uninstall.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-helm uninstall deeparcher-mongodb-sharded
+helm uninstall deeparcher-mongodb-sharded && \
+kubectl delete pvc datadir-deeparcher-mongodb-sharded-shard0-data-0 && \
+kubectl delete pvc datadir-deeparcher-mongodb-sharded-shard1-data-0 && \
+kubectl delete pvc datadir-deeparcher-mongodb-sharded-shard2-data-0 && \
+kubectl delete pvc datadir-deeparcher-mongodb-sharded-configsvr-0 && \
+kubectl delete -f pv.yaml
