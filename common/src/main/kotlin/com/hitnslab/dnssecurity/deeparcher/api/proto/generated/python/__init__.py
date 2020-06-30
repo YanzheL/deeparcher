89f77f160e3296bdda8a9f3d44a9dba9746b4452
YanzheL <lee.yanzhe@yanzhe.org>
2020-06-30 23:58:36 +0800

    Add __init__.py for generated python files
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

---
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/api/proto/generated/python/__init__.py
@@ -0,0 +1,9 @@
+from .domain_dns_detail_pb2 import DomainDnsDetail
+from .graph_assoc_edge_update_pb2 import GraphAssocEdgeUpdate
+from .graph_attributes_pb2 import GraphElementsAttrMap, ComponentAttr
+from .graph_event_pb2 import GraphEvent
+from .graph_pb2 import Graph
+from .known_malicious_domain_pb2 import KnownMaliciousDomain
+from .matrix_pb2 import CooMatFloat, CooMatInt64, CscMatFloat, CscMatInt64, CsrMatFloat, CsrMatInt64, DenseMatFloat, \
+    DenseMatInt64
+from .pdns_data_pb2 import PDnsData
