4c154a46583b28ba76f227d9bcf1b73c1044e355
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-09 10:01:28 +0800

    Implement new Analyzer interface
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/app/analyzer/malicious_association_analyzer.py
+++ graph-analyzer/app/analyzer/malicious_association_analyzer.py
@@ -1,119 +1,43 @@
 import math
-from queue import Queue
 from typing import *
 
 import cudf
 import cugraph
 import cupy
 import numpy as np
-from cugraph.components.connectivity import weakly_connected_components
 from cugraph.traversal.sssp import sssp
 
-from app.analyzer.interface import GraphAnalyzer, GraphAttrExtractor
-from app.struct import NodeAttrMap, Graph
-from app.util.misc import timing, load_blacklist, extract_labels
+from app.analyzer.interface import GraphAnalyzer
+from app.io.graph_converter import build_cugraph
+from app.struct import Graph
+from app.util.misc import timing, extract_bool_attr_ids
 
 
-class MaliciousAssociationAnalyzer(GraphAnalyzer, GraphAttrExtractor):
+class MaliciousAssociationAnalyzer(GraphAnalyzer):
 
-    def __init__(
-            self,
-            graph: Graph,
-            blacklist: Optional[str] = None,
-            cc_size_thres=1
-    ):
-        super(GraphAnalyzer).__init__(graph)
-        self._blacklist: Optional[np.ndarray] = None
-        if blacklist is not None:
-            self._blacklist = load_blacklist(blacklist, self.logger)
-        self._node_labels: np.ndarray = extract_labels('fqdn', self.graph.node_attrs, self.graph.nodes)
-        self._cc_size_thres = cc_size_thres
-        self._cugraph: cugraph.DiGraph = self._build_cugraph()
-        self._black_node_ids: Optional[np.ndarray] = None
-        self._components: List[cudf.Series] = []
-        self._scores: Dict[int, float] = {}
-        self._task_queue = Queue()
-        self._finished = False
-
-    def is_finished(self) -> bool:
-        return self._finished
-
-    def get_node_attrs(self, **kwargs) -> List[NodeAttrMap]:
-        return [NodeAttrMap('mal_assoc_prob', self._scores)]
-
-    def reset(self) -> NoReturn:
-        self._scores.clear()
-        self._finished = False
-
-    def is_restartable(self) -> bool:
-        return True
-
-    @timing
-    def _run(self, blacklist: Optional[str] = None) -> NoReturn:
-        if blacklist is not None:
-            self._blacklist = load_blacklist(blacklist, self.logger)
-            self._black_node_ids = None
-        if self._black_node_ids is None:
-            self.logger.info('Computing black node ids...')
-            self._black_node_ids = np.intersect1d(
-                self._node_labels, self._blacklist, assume_unique=True, return_indices=True
-            )[1]
-            self.logger.info('Found {} black nodes in the whole graph'.format(self._black_node_ids.size))
-            for id in self._black_node_ids:
-                self._scores[id] = 1.0
-        if self.graph.connected or len(self._compute_connected_components()) == 1:
-            self.logger.info('The input graph is connected.')
-            self._run_for_connected_graph(self._cugraph, self.graph.node_id_remap)
-        else:
-            for idx, cc in enumerate(self._components):
-                if len(cc) <= self._cc_size_thres:
-                    continue
-                adj_list = cugraph.subgraph(self._cugraph, cc).view_edge_list()
-                edges = cudf.DataFrame()
-                edges['src'], edges['dst'], node_id_remap = cugraph.renumber(adj_list['src'], adj_list['dst'])
-                edges['weight'] = adj_list['weight']
-                subgraph = cugraph.DiGraph()
-                subgraph.from_cudf_edgelist(edges, 'src', 'dst', 'weight')
-                self.logger.info(
-                    'Begin analyzing current connected subgraph {}.'.format(self.__class__, idx))
-                self._run_for_connected_graph(subgraph, node_id_remap)
-        self._finished = True
-
-    @timing
-    def _build_cugraph(self) -> cugraph.DiGraph:
-        adj = self.graph.adj.tocoo()
-        df = cudf.DataFrame()
-        df['src'] = adj.row
-        df['dst'] = adj.col
-        df['weight'] = adj.data
-        g = cugraph.DiGraph()
-        g.from_cudf_edgelist(df, 'src', 'dst', 'weight')
-        # adj = self.graph.adj.tocsr()
-        # offsets = cudf.Series(adj.indptr)
-        # indices = cudf.Series(adj.indices)
-        # g.from_cudf_adjlist(offsets, indices, None)
-        return g
-
-    @timing
-    def _run_for_connected_graph(self, graph: cugraph.Graph, node_id_remap: Optional[np.ndarray] = None) -> NoReturn:
-        if node_id_remap is not None:
-            seeds = np.intersect1d(node_id_remap, self._black_node_ids, assume_unique=True, return_indices=True)[1]
-        else:
-            seeds = self._black_node_ids
+    def analyze(self, graph: Graph, ctx: dict, **runtime_configs) -> Union[Graph, List[Graph], None]:
+        if not graph.connected:
+            self.logger.warn('Current graph is not connected, now skipped.')
+            return []
+        if 'cugraph' not in graph.meta:
+            graph.meta['cugraph'] = build_cugraph(graph.adj)
+        cug = graph.meta['cugraph']
+        seeds, _ = extract_bool_attr_ids('black_or_white', graph.node_attrs)
+        scores: Dict[int, float] = {s: 1.0 for s in seeds}
         if seeds.size <= 1:
-            self.logger.info('Skipped a trivial subgraph. It has only {} seeds in {} nodes.'.format(
-                seeds.size, graph.number_of_nodes()))
+            self.logger.info('Skipped a trivial graph. It has only {} seeds in {} nodes.'.format(
+                seeds.size, graph.nodes))
             return
-        self._initialize_weights(graph)
+        self._initialize_weights(cug)
         dists = []
         # np.vectorize(lambda s: sssp(graph,s)['distance'].to_array(),signature='()->(n)')
         for seed in seeds:
-            dist = cupy.asarray(sssp(graph, seed)['distance'].data)
+            dist = cupy.asarray(sssp(cug, seed)['distance'].data)
             dists.append(dist)
         dists = cupy.asarray(dists).T  # shape = (nodes, seeds)
-        scores = self._compute_mal_scores(dists)
-        self._scores.update(zip(node_id_remap, scores))
-        self.logger.info('Updated {} scores'.format(scores.size))
+        scores.update(zip(graph.node_id_remap, self._compute_mal_scores(dists)))
+        self.logger.info('Updated {} scores'.format(len(scores)))
+        graph.node_attrs['mal_assoc_prob'] = scores
 
     @staticmethod
     @timing
@@ -129,27 +53,6 @@ class MaliciousAssociationAnalyzer(GraphAnalyzer, GraphAttrExtractor):
             coef = cupy.logspace(n_seeds - 1, 2 - 1, num=n_seeds - 1, base=0.5, dtype=np.float64)
             mal += (1 - largest_assoc) * cupy.sum(coef * assocs[:, 1:], axis=1)
         return mal
-
-    @timing
-    def _compute_connected_components(self) -> List[cudf.Series]:
-        if self.graph.component_attrs is None:
-            self.logger.info('Computing weakly connected components...')
-            cc_df = weakly_connected_components(self._cugraph)['labels']
-            n_components = cc_df.max()
-            self.logger.info('Got {} weakly connected components.'.format(n_components))
-            if n_components == 1:
-                self.graph.connected = True
-                self._components.append(self._cugraph.nodes())
-            else:
-                for i in range(1, cc_df.max() + 1):
-                    component = cc_df.query("labels == {}".format(i))['vertices']
-                    self._components.add(component)
-        else:
-            self.logger.info('Found {} precomputed weakly connected components in graph attributes.'.format(
-                len(self.graph.component_attrs)))
-            for cc in self.graph.component_attrs:
-                self._components.append(cudf.Series(cc.components, dtype=np.int32))
-        return self._components
 
     @staticmethod
     def _initialize_weights(graph: cugraph.Graph):
--- graph-analyzer/app/analyzer/topology_based_flow_analyzer.py
+++ graph-analyzer/app/analyzer/topology_based_flow_analyzer.py
@@ -1,27 +1,14 @@
 from typing import *
 
-import numpy as np
-
-from app.analyzer.interface import GraphAnalyzer, GraphAttrExtractor
-from app.struct import NodeAttrMap, Graph
-from app.util.misc import load_blacklist, load_whitelist
+from app.analyzer.interface import GraphAnalyzer
+from app.struct import Graph
+from app.util.misc import extract_bool_attr_ids
 
 
-class TopologyBasedFlowAnalyzer(GraphAnalyzer, GraphAttrExtractor):
+class TopologyBasedFlowAnalyzer(GraphAnalyzer):
 
-    def __init__(self, graph: Graph, whitelist: str, blacklist: str):
-        super(GraphAnalyzer).__init__(graph)
-        self.whitelist: np.ndarray = load_whitelist(whitelist, self.logger)
-        self.blacklist: np.ndarray = load_blacklist(blacklist, self.logger)
-        self._scores: Dict[int, float] = {}
-        self._finished = False
-
-    def _run(self, **runtime_configs) -> NoReturn:
-        # TODO: Implement the algorithm here
-        self._finished = True
-
-    def is_finished(self) -> bool:
-        return self._finished
-
-    def get_node_attrs(self, **kwargs) -> List[NodeAttrMap]:
-        return [NodeAttrMap('tbf_prob', self._scores)]
+    def analyze(self, graph: Graph, ctx: dict, **runtime_configs) -> Union[Graph, List[Graph], None]:
+        black_node_ids, white_node_ids = extract_bool_attr_ids('black_or_white', graph.node_attrs)
+        scores: Dict[int, float] = {}
+        # TODO: Implement it here.
+        graph.node_attrs['tbf_prob'] = scores
