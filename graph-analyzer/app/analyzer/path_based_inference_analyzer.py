079b9ee06c48f0a1b6a96f9cf90e8838fc82f758
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-12 07:38:44 +0800

    Tested PathBasedInferenceAnalyzer
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/app/analyzer/path_based_inference_analyzer.py
+++ graph-analyzer/app/analyzer/path_based_inference_analyzer.py
@@ -1,19 +1,17 @@
 from __future__ import annotations
 
-import math
-from typing import TYPE_CHECKING, Optional, Dict, NoReturn
+from typing import TYPE_CHECKING, Optional, Dict
 
 if TYPE_CHECKING:
     from app.struct import Graph
 
-import cudf
-import cugraph
 import cupy
 from cugraph.traversal.sssp import sssp
 
 from app.analyzer.interface import GraphAnalyzer
 from app.io.graph_converter import build_cugraph
 from app.util.misc import extract_bool_attr_ids
+import math
 
 
 class PathBasedInferenceAnalyzer(GraphAnalyzer):
@@ -50,23 +48,24 @@ class PathBasedInferenceAnalyzer(GraphAnalyzer):
 
         """
 
-        if 'cu_graph' not in graph.meta:
-            graph.meta['cu_graph'] = build_cugraph(graph.adj)
-        cu_graph = graph.meta['cu_graph']
+        cu_graph = build_cugraph(graph.adj, weight_transform=lambda x: math.log(x + 1 / x))
         seeds, _ = extract_bool_attr_ids(bw_attr, graph.node_attrs)
-        if seeds.size <= 1:
-            self.logger.info('Skipped a trivial graph. It has only {} seeds in {} nodes.'.format(
-                seeds.size, graph.nodes))
+        if seeds.size < 1:
             return
-        self._transform_weights(cu_graph)
-        # np.vectorize(lambda s: sssp(graph,s)['distance'].to_array(),signature='()->(n)')
+        self.logger.info("Found {} black nodes in {} nodes".format(seeds.size, graph.nodes))
         dists = [
-            cupy.asarray(sssp(cu_graph, seed)['distance'].data)
+            cupy.asarray(sssp(cu_graph, seed)['distance'])
             for seed in seeds
         ]
         dists = cupy.asarray(dists).T  # shape = (nodes, seeds)
-        scores: Dict[int, float] = dict(enumerate(self._compute_mal_scores(dists)))
-        self.logger.info('Updated {} scores'.format(len(scores)))
+        dists[dists > 1e14] = cupy.inf
+        # self.logger.info('dists = \n{}'.format(dists))
+        mal = self._compute_mal_scores(dists)
+        # if not (mal[seeds] == 1.0).all():
+        #     self.logger.warn("PBI result maybe invalid.")
+        #     self.logger.warn('mal = \n{}'.format(mal))
+        scores: Dict[int, float] = dict(enumerate(mal))
+        self.logger.info('Updated {} scores'.format(mal.size))
         graph.node_attrs[dst_attr] = scores
         return graph
 
@@ -77,17 +76,12 @@ class PathBasedInferenceAnalyzer(GraphAnalyzer):
     def _compute_mal_scores(dists: cupy.ndarray) -> cupy.ndarray:
         n_seeds = dists.shape[1]
         assocs = dists
-        assocs *= -1
+        assocs = cupy.multiply(assocs, -1, out=assocs)
         cupy.exp(assocs, assocs)
-        assocs.sort(int_axis=1)
+        assocs = cupy.sort(assocs, axis=1)
         largest_assoc = assocs[:, -1]
         mal = largest_assoc
         if n_seeds > 2:
             coef = cupy.logspace(n_seeds - 1, 2 - 1, num=n_seeds - 1, base=0.5, dtype=cupy.float32)
             mal += (1 - largest_assoc) * cupy.sum(coef * assocs[:, 1:], axis=1)
         return mal
-
-    @staticmethod
-    def _transform_weights(graph: cugraph.Graph) -> NoReturn:
-        weights: cudf.Series = graph.view_edge_list()['weight']
-        graph.view_edge_list()['weight'] = weights.applymap(lambda x: math.log(x + 1 / x))
