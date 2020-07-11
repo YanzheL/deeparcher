1fbf4f1685b69aa2220c3416e83694d6957def93
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-12 02:38:34 +0800

    Fix type hints
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/app/analyzer/interface.py
+++ graph-analyzer/app/analyzer/interface.py
@@ -1,5 +1,6 @@
-from abc import ABCMeta, abstractmethod
+from __future__ import annotations
 
+from abc import ABCMeta, abstractmethod
 from typing import TYPE_CHECKING, List, Union, NoReturn
 
 if TYPE_CHECKING:
