5ead6ddba0af160b5cbf233520ac7ee1b107c4f1
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-12 07:39:57 +0800

    Add optional weight_transform arg.
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/app/io/graph_converter.py
+++ graph-analyzer/app/io/graph_converter.py
@@ -10,16 +10,18 @@ from app.util.logger_router import LoggerRouter
 DEFAULT_LOGGER = LoggerRouter().get_logger(__name__)
 
 
-def build_cugraph(adj: 'spmatrix', symmetrized=True) -> 'cugraph.Graph':
+def build_cugraph(adj: 'spmatrix', symmetrized=True, weight_transform=None) -> 'cugraph.Graph':
     import cudf
     import cugraph
     adj = adj.tocoo()
     df = cudf.DataFrame()
     df['src'] = adj.row
     df['dst'] = adj.col
-    df['weight'] = adj.data
+    df['weights'] = adj.data
+    if weight_transform is not None:
+        df['weights'] = df['weights'].applymap(weight_transform)
     g = cugraph.Graph(symmetrized=symmetrized)
-    g.from_cudf_edgelist(df, 'src', 'dst', 'weight')
+    g.from_cudf_edgelist(df, 'src', 'dst', 'weights')
     # adj = self.graph.adj.tocsr()
     # offsets = cudf.Series(adj.indptr)
     # indices = cudf.Series(adj.indices)
