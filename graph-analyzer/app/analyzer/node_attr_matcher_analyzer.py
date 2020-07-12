fa459c3952b10370886bd6e2066d215e18f7089a
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-12 09:37:58 +0800

    Tested NodeAttrMatcherAnalyzer
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/app/analyzer/node_attr_matcher_analyzer.py
+++ graph-analyzer/app/analyzer/node_attr_matcher_analyzer.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Callable
 
 if TYPE_CHECKING:
     from app.struct import Graph
@@ -11,7 +11,7 @@ from app.analyzer.interface import GraphAnalyzer
 
 
 class NodeAttrMatcherAnalyzer(GraphAnalyzer):
-    """Match the content of a node attribute with pre-configured true lists and false lists.
+    """Match the content of a node attribute against pre-configured true list and false list.
 
     Author:
         Yanzhe Lee <lee.yanzhe@yanzhe.org>
@@ -39,8 +39,16 @@ class NodeAttrMatcherAnalyzer(GraphAnalyzer):
                 self._true_list = np.setdiff1d(self._true_list, common, assume_unique=True)
                 self._false_list = np.setdiff1d(self._false_list, common, assume_unique=True)
 
-    def analyze(self, graph: Graph, ctx: dict, src_attr: str, dst_attr: str) -> Graph:
-        """Match the content of a node attribute named `src_attr` with pre-configured true lists and false lists.
+    def analyze(
+            self,
+            graph: Graph,
+            ctx: dict,
+            src_attr: str,
+            dst_attr: str,
+            value_true_mapper: Callable[[object], object] = None,
+            value_false_mapper: Callable[[object], object] = None
+    ) -> Graph:
+        """Match the content of a node attribute named `src_attr` against pre-configured true list and false list.
 
         Analyzed result will be written to graph.node_attrs[dst_attr] with type Dict[int, bool].
         For each (id,value) pair in graph.node_attrs[src_attr],
@@ -52,6 +60,10 @@ class NodeAttrMatcherAnalyzer(GraphAnalyzer):
             ctx: Shared analyzer context.
             src_attr: Name of an existing string node attribute in the graph.
             dst_attr: Name of the output node attribute.
+            value_true_mapper: A custom function that will be applied to each value of graph.node_attrs[src_attr]
+                before matching against true_list.
+            value_false_mapper: A custom function that will be applied to each value of graph.node_attrs[src_attr]
+                before matching against false_list.
 
         Returns:
             Graph: An analyzed graph which contains an output node attribute.
@@ -59,19 +71,27 @@ class NodeAttrMatcherAnalyzer(GraphAnalyzer):
         """
         src = graph.node_attrs[src_attr]
         dst_data: Dict[int, bool] = {}
-        if len(src) == graph.nodes:
-            values = np.array(
-                [v for k, v in sorted(src.items())]
-            )
-            true_ids = np.intersect1d(values, self._true_list, return_indices=True)[1]
-            false_ids = np.intersect1d(values, self._false_list, return_indices=True)[1]
+        if len(src) == graph.nodes:  # Fast path.
+            values = [v for k, v in sorted(src.items())]
+            values_true = values
+            values_false = values
+            if value_true_mapper is not None:
+                values_true = list(map(value_true_mapper, values_true))
+            if value_false_mapper is not None:
+                values_false = list(map(value_false_mapper, values_false))
+            true_ids = np.intersect1d(values_true, self._true_list, return_indices=True)[1]
+            false_ids = np.intersect1d(values_false, self._false_list, return_indices=True)[1]
             dst_data.update(map(lambda i: (i, True), true_ids))
             dst_data.update(map(lambda i: (i, False), false_ids))
         else:
+            if value_true_mapper is None:
+                value_true_mapper = lambda x: x
+            if value_false_mapper is None:
+                value_false_mapper = lambda x: x
             for node_id, value in src:
-                if value in self._true_list:
+                if value_true_mapper(value) in self._true_list:
                     dst_data[node_id] = True
-                elif value in self._false_list:
+                elif value_false_mapper(value) in self._false_list:
                     dst_data[node_id] = False
         graph.node_attrs[dst_attr] = dst_data
         return graph
