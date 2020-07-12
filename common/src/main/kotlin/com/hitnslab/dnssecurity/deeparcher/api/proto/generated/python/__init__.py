7893f78470843d73ad3a78cd9bad6d499300b103
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-12 16:45:17 +0800

    Fix proto imports
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/api/proto/generated/python/__init__.py
+++ common/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/api/proto/generated/python/__init__.py
@@ -4,6 +4,6 @@ from .graph_attributes_pb2 import GraphElementsAttrMap, ComponentAttr
 from .graph_event_pb2 import GraphEvent
 from .graph_pb2 import Graph
 from .known_malicious_domain_pb2 import KnownMaliciousDomain
-from .matrix_pb2 import CooMatFloat, CooMatInt64, CscMatFloat, CscMatInt64, CsrMatFloat, CsrMatInt64, DenseMatFloat, \
-    DenseMatInt64
+from .matrix_pb2 import CooMatFloat, CscMatFloat, CsrMatFloat, DenseMatFloat, CooMatInt32, CscMatInt32, CsrMatInt32, \
+    DenseMatInt32
 from .pdns_data_pb2 import PDnsData
