b97b6246e38c5f6650a581271b9ebfb51677b85d
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-13 02:18:35 +0800

    Define ObjectIdService interface
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

---
+++ stream/src/main/kotlin/com/hitnslab/dnssecurity/deeparcher/stream/service/ObjectIdService.kt
@@ -0,0 +1,24 @@
+package com.hitnslab.dnssecurity.deeparcher.stream.service
+
+interface ObjectIdService<K> {
+
+    fun getCurrentMaxId(): Long
+
+    fun getExistingId(key: K): Long?
+
+    fun getOrCreateId(key: K): Long
+
+    fun getAllExistingEntries(): Sequence<Map.Entry<K, Long>>
+
+    fun getOrCreateIds(keys: Iterable<K>): Sequence<Long> {
+        return keys.asSequence().map { k -> getOrCreateId(k) }
+    }
+
+    fun commit(): Boolean {
+        return true
+    }
+
+    fun close(): Boolean {
+        return true
+    }
+}
