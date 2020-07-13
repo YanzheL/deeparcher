51ba88e58ffd3a1ac9f364ce870e88f0b97da697
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-14 05:49:07 +0800

    Do not use symlink

---
+++ graph-analyzer/app/struct/proto/__init__.py
@@ -0,0 +1,8 @@
+from .domain_dns_detail_pb2 import DomainDnsDetail
+from .graph_attributes_pb2 import GraphElementsAttrMap, ComponentAttr
+from .graph_event_pb2 import GraphEvent
+from .graph_pb2 import Graph
+from .known_malicious_domain_pb2 import KnownMaliciousDomain
+from .matrix_pb2 import CooMatFloat, CscMatFloat, CsrMatFloat, DenseMatFloat, CooMatInt32, CscMatInt32, CsrMatInt32, \
+    DenseMatInt32
+from .pdns_data_pb2 import PDnsData
