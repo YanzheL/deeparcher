019fcb824fd8837c686de38f9d1f4afe81b69ca6
YanzheL <lee.yanzhe@yanzhe.org>
2020-07-14 15:55:51 +0800

    Refine
    Signed-off-by: YanzheL <lee.yanzhe@yanzhe.org>

--- graph-analyzer/main.py
+++ graph-analyzer/main.py
@@ -14,6 +14,9 @@ from functools import partial
 from importlib import import_module
 from queue import Queue
 import pandas as pd
+from app.util.logger_router import LoggerRouter
+
+logger = LoggerRouter().get_logger(__name__)
 
 
 def load_analyzer_instances(name: str, *args, **kwargs) -> GraphAnalyzer:
@@ -30,7 +33,8 @@ def load_graph(type: str, path: str) -> Graph:
 
 
 def load_inputs(graph, blacklist, whitelist) -> Tuple[Graph, np.ndarray, np.ndarray]:
-    return load_graph(**graph), load_blacklist(blacklist['path']), load_whitelist(whitelist['path'])
+    return load_graph(**graph), load_blacklist(blacklist['path'], blacklist['validate']), load_whitelist(
+        whitelist['path'], whitelist['validate'])
 
 
 def load_yaml(path: str) -> dict:
@@ -51,14 +55,17 @@ def dump_node_attributes(graph: Graph, shared_data: Dict[str, np.ndarray], ignor
 
 def main(inputs, output, analyzers):
     initial_graph, blacklist, whitelist = load_inputs(**inputs)
+    logger.info("Loaded inputs")
     pipeline: List[Tuple[GraphAnalyzer, dict]] = [(load_analyzer_instances(analyzer['name']), analyzer['config'])
                                                   for analyzer in analyzers if analyzer['enable']]
+    logger.info("Loaded analyzers")
     # return
     context = {}
     initial_analyzer = NodeAttrMatcherAnalyzer(
         blacklist,
         whitelist
     )
+    logger.info("Loaded initial_analyzer")
     initial_graph = initial_analyzer.analyze(
         initial_graph,
         context,
@@ -66,7 +73,7 @@ def main(inputs, output, analyzers):
         dst_attr='black_or_white',
         value_false_mapper=partial(get_fld, fail_silently=True, fix_protocol=True)
     )
-
+    logger.info("Analyzed initial_graph")
     graphs = Queue()
     initial_graph.meta['initial'] = True
     graphs.put(initial_graph)
@@ -83,7 +90,7 @@ def main(inputs, output, analyzers):
             else:
                 graph = ret
         analyzed.put(graph)
-
+    logger.info("Analysis finished")
     all_attributes: Dict[str, np.ndarray] = {}
     while not graphs.empty():
         graph = analyzed.get()
@@ -93,9 +100,10 @@ def main(inputs, output, analyzers):
         dump_node_attributes(graph, all_attributes, ignored_attrs)
     df = pd.DataFrame(data=all_attributes)
     df.to_csv(output['path'])
+    logger.info("Output finished")
 
 
 if __name__ == '__main__':
     app_config = load_yaml('application.yml')['app']
-    print(app_config)
+    logger.debug(f"Loaded config = \n{app_config}")
     main(**app_config)
